8dc3c0199be97b498e4c1a30cc834193d28172f3
cinqho <cinqho@gmail.com>
2026-03-07 00:47:14 +0100

    fix: supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,4 +2,11 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-export const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  db: { schema: 'public' },
+  auth: { persistSession: true, autoRefreshToken: true },
+  global: {
+    headers: { 'x-connection-encrypted': 'true' },
+  },
+})
